a4e804b382d3cb516fcf067daa69c94bb48d4dfa
SyedAsadKazmi <syedasadkazmi786@gmail.com>
2022-04-09 14:43:36 +0530

    Initial commit

--- AggregatorV2V3Interface.sol
+++ AggregatorV2V3Interface.sol
@@ -1,7 +1,7 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.7.0;
 
-import "./AggregatorInterface.sol";
+import "@chainlink/contracts/src/v0.7/AggregatorV3Interface.sol";
 import "./AggregatorV3Interface.sol";
 
 interface AggregatorV2V3Interface is AggregatorInterface, AggregatorV3Interface
